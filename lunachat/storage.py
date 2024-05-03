ad56010cc0e330d6ba8a2171e73338112842e4ee
Luna <luna@lunaskye.net>
2024-05-03 14:53:08 -0700

    Refinements to s3 storage impl

--- lunachat/storage.py
+++ lunachat/storage.py
@@ -1,17 +1,10 @@
-import json
+import json, pprint
 import streamlit as st
 from st_files_connection import FilesConnection
 
 conn = st.connection("s3", protocol="s3", type=FilesConnection)
 
-test_file = "lunaskye-lunachat/test.txt"
 system_prompts_file = "lunaskye-lunachat/system_prompts.json"
-
-try:
-    _ = conn.read(test_file, input_format="text")
-except FileNotFoundError:
-    with conn.open(test_file, "wt") as f:
-        f.write("This is a test")
 
 
 def save(path: str, data: dict) -> bool:
@@ -22,3 +15,19 @@ def save(path: str, data: dict) -> bool:
 def load(path: str) -> dict:
     json_data = conn.read(path, input_format="text")
     return json.loads(json_data)
+
+
+# Verify file
+default_json_data = json.dumps({"default": "You are a helpful assistant."})
+try:
+    file_data = conn.read(system_prompts_file, input_format="text")
+    if not file_data:
+        print("File has no content, writing default.")
+        save(system_prompts_file, default_json_data)
+except FileNotFoundError:
+    print("File not found, writing default.")
+    save(system_prompts_file, default_json_data)
+
+if __name__ == "__main__":
+    data = load(system_prompts_file)
+    print(data)
